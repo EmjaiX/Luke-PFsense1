5d21f30567ef5bef5c77d91d5b82077e2c1c0162
Lexan E Flowers <lexar5657@gmail.com>
2022-06-29 14:43:13 -0600

    update,files

---
+++ index.php
@@ -0,0 +1,35 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Login</title>
+<meta name="generator" content="WYSIWYG Web Builder 17 - https://www.wysiwygwebbuilder.com">
+<link href="Untitled1.css" rel="stylesheet">
+<link href="index.css" rel="stylesheet">
+</head>
+<body>
+<div id="PageHeader1" style="position:absolute;text-align:left;left:0px;top:0px;width:100%;height:71px;z-index:7777;">
+<div id="wb_Image2" style="position:absolute;left:13px;top:12px;width:70px;height:49px;z-index:0;">
+<img src="images/san-pedro-belize-express-logo.png" id="Image2" alt="" width="70" height="49"></div>
+</div>
+
+<div id="wb_Image1" style="position:absolute;left:10px;top:72px;width:363px;height:178px;z-index:3;">
+<img src="Belize-Ferry-Ride.jpeg" id="Image1" alt="" width="363" height="179"></div>
+<div id="wb_Image3" style="position:absolute;left:136px;top:281px;width:110px;height:77px;z-index:4;">
+<img src="images/wifi.png" id="Image3" alt="" width="110" height="78"></div>
+<input type="submit" id="Button1" name="" value="Login" style="position:absolute;left:128px;top:705px;width:126px;height:59px;z-index:5;">
+<div id="wb_MediaPlayer1" style="position:absolute;left:16px;top:379px;width:350px;height:294px;z-index:6;">
+<video id="MediaPlayer1" autoplay controls preload="auto" playsinline>
+<source src="San Pedro Belize Express-480p.mp4" type="video/mp4">
+<source src="Belize Express water taxi schedule & dock.mp4" type="video/mp4">
+<source src="DG VO   SAN PEDRO BELIZE EXPRESS   THE BEST.mp4" type="video/mp4">
+<source src="Let's Go Belizing with San Pedro Belize Express!-480p.mp4" type="video/mp4">
+<source src="San Pedro Belize Express - GENERIC AD-480p.mp4" type="video/mp4">
+<source src="SAN PEDRO BELIZE EXPRESS WATER TAXI - MEMBERSHIP CARD.mp4" type="video/mp4">
+<source src="San Pedro Belize Express-480p.mp4" type="video/mp4">
+</video>
+</div>
+<div id="PageFooter1" style="position:absolute;overflow:hidden;text-align:left;left:0px;top:999px;width:100%;height:100px;z-index:7;">
+</div>
+</body>
+</html>
